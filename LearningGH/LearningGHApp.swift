c205c4d541a486d1348cb22e2a0df1d58f5f84db
Koreanhoon <ryanhoonseok@gmail.com>
2022-09-07 19:23:06 +0900

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Ryan Lee on 2022/09/07.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
